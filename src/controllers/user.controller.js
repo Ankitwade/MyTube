06e805166fb545a6770a9191fe27bb5b9133df57
Ankit <ankitwade1612@gmail.com>
2026-02-12 01:18:17 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,79 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js';
+import {User} from '../models/user.model.js';
+import {uplodOnClodinary} from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+
 
 const registerUser = asyncHandler(async (req , res )=>{
-    res.status(200).json({
-        message:"ok"
+    // STEPS for user registration
+    // get user details from frontend
+    // validation - if empty?
+    // check if user already exixt - username, email
+    // check images - check for avatar
+    // uplod them to cloudnary, avatar 
+    // create user object - create entry in db
+    // remove password and refresh token field from response 
+    // check for user creation 
+    // return res
+
+    const {fullname, email, username , password} = req.body
+    console.log("Email: ", email);
+
+    // get user details from frontend
+    if(
+        [fullname, email, username, password].some((field)=>field?.trim()==="")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+    
+    // If user already exixt throw error
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409, "User with username or email already exists")
+    }
+
+    // checking the status of avatar and coverImage
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    // uploding on cloudinay
+    if (!avatarLocalPath) {
+        throw new ApiError(400 , "Avatar image is required")
+    }
+
+    const avatar = await uplodOnClodinary(avatarLocalPath)
+    const coverImage = await uplodOnClodinary(coverImageLocalPath)
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400 , "Avatar image is required")
+    }
+
+    // uploding data in DB
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    // checking the status of user into DB by id
+    const createdUser = await user.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if (!createdUser) {
+        throw new ApiError(500 , "Something went wrong while registering the user")
+    }
+
+    //returning response
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered sucessfully")
+    )
 })
 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,7 +30,7 @@ const userSchema = new Schema(
             requied: true,
         },
         coverImage: {
-            type : true,
+            type : String,
         },
         watchHistory: [{
             type : Schema.Types.ObjectId,
@@ -49,7 +49,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function(next){
     if(!this.isModified("password"))return next();
 
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
